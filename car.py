9195e92eeae70b2d9426d43a1c931c7fc4b14771
Wanda Wang <chuwang@wustl.edu>
2022-06-25 14:45:07 -0700

    Refactor Engine, Battery, Car, and Serviceable classes

--- car.py
+++ car.py
@@ -1,10 +1,9 @@
-from abc import ABC, abstractmethod
+from abc import ABC
+
+from serviceable import serviceable
 
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
-    @abstractmethod
-    def needs_service(self):
-        pass
+class Car(Serviceable, ABC):
+    def __init__(self, engine, battery):
+        self.engine = engine
+        self.battery = battery
