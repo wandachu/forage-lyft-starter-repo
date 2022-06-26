2d315861b0282c56d14396da230281a6bc0ea163
Wanda Wang <chuwang@wustl.edu>
2022-06-25 17:36:52 -0700

    Update car factory methods to include tire

--- car.py
+++ car.py
@@ -2,6 +2,7 @@ from serviceable import serviceable
 
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tire):
         self.engine = engine
         self.battery = battery
+        self.tire = tire
--- car_factory.py
+++ car_factory.py
@@ -5,30 +5,38 @@ from component.engine.willoughby_engine import WilloughbyEngine
 from component.battery.spindler_battery import SpindlerBattery
 from component.battery.nubbin_battery import NubbinBattery
 
+from component.tire.carrigan_tire import CarriganTire
+from component.tire.octoprime_tire import OctoprimeTire
+
 
 class CarFactory(ABC):
 
-    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_calliope(self, current_date, last_service_date, current_mileage, last_service_mileage, sensor_array):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tire = CarriganTire(sensor_array)
+        return Car(engine, battery, tire)
 
-    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def create_glissade(self, current_date, last_service_date, current_mileage, last_service_mileage, sensor_array):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = SpindlerBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tire = CarriganTire(sensor_array)
+        return Car(engine, battery, tire)
 
-    def create_palindrome(self, current_date, last_service_date, warning_light_on):
+    def create_palindrome(self, current_date, last_service_date, warning_light_on, sensor_array):
         engine = SternmanEngine(warning_light_on)
         battery = SpindlerBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tire = CarriganTire(sensor_array)
+        return Car(engine, battery, tire)
 
-    def  create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def  create_rorschach(self, current_date, last_service_date, current_mileage, last_service_mileage, sensor_array):
         engine = WilloughbyEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tire = OctoprimeTire(sensor_array)
+        return Car(engine, battery, tire)
 
-    def  create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage):
+    def  create_thovex(self, current_date, last_service_date, current_mileage, last_service_mileage, sensor_array):
         engine = CapuletEngine(current_mileage, last_service_mileage)
         battery = NubbinBattery(last_service_date, current_date)
-        return Car(engine, battery)
+        tire = OctoprimeTire(sensor_array)
+        return Car(engine, battery, tire)
